1b11f242c27c6ce951a74982a00c7cb37c5855b7
PSEH-7 <39406614+PSEH-7@users.noreply.github.com>
2019-10-12 14:16:26 +0530

    Update FootballApiApplication.java

--- FootballAPI/src/main/java/com/football/main/FootballApiApplication.java
+++ FootballAPI/src/main/java/com/football/main/FootballApiApplication.java
@@ -2,9 +2,12 @@ package com.football.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.football.controller.FootballController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = FootballServiceImpl.class)
+@ComponentScan(basePackageClasses = FootballController.class)
 public class FootballApiApplication {
 
 	public static void main(String[] args) {
